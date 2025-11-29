80132d62785b524bfff3212ca529eea6e3250756
HSKYOO <hyunseok888@gmail.com>
2025-11-29 19:21:26 +0900

    app.js updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ShowList from "./components/ShowList";
+import CreatePage from "./components/CreatePage";
+import UpdatePage from "./components/UpdatePage";
+import DetailPage from "./components/DetailPage";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<ShowList />} />
+        <Route path='/list' element={<ShowList />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/detail/:id" element={<DetailPage />} />
+        <Route path="/update/:id" element={<UpdatePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
